a91955cf68671f8603219567d138e51f6157cfef
linweisen <linws.sai@gmail.com>
2022-03-31 22:14:23 +0800

    make兼容mac和linux

--- kernel/main.c
+++ kernel/main.c
@@ -1,8 +1,6 @@
-//
-// Created by linweisen on 2022/3/28.
-//
+#include <stdio.h>
+
 int main(void) {
-    while(1);
     return 0;
 }
 
--- tools/build.sh
+++ tools/build.sh
@@ -24,7 +24,7 @@ fi
 
 # Write bootsect (512 bytes, one sector) to stdout
 [ ! -f "$bootsect" ] && echo "there is no bootsect binary file there" && exit -1
-dd if=$bootsect bs=512 count=1 of=$IMAGE 2>&1 >/dev/null
+ndisasm -o 0x7C00 boot.bin >> disboot.asm
 
 # Write setup(4 * 512bytes, four sectors) to stdout
 [ ! -f "$setup" ] && echo "there is no setup binary file there" && exit -1
